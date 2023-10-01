ddb8f1ca244cb3dd600424e650c137f543d3bf13
AigulY <azia.kz.a@gmail.com>
2023-10-01 06:28:35 -0400

    lesson_1_1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import React from 'react';
 
+const todoList = [
+  { 
+    id: 1, 
+    title: "Complete assignment",
+    assignment_number: "_1_1"
+  },
+  { 
+    id: 2, 
+    title: "Git push assignment",
+    assignment_number: "_1_1" 
+  },
+  { 
+    id: 3, 
+    title: "Merge assignment",
+    assignment_number: "_1_1" 
+  },
+];
+
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ToDo List</h1>
+      <label htmlFor='search'>Search: </label>
+        <input id='search' type='text'></input>
+      
+      <hr />
+      {/* render the list */}
+      <ul>
+        {todoList.map(function(item) {
+          return (
+            <li key={item.id}>
+              <span>
+                { item.title }
+              </span>
+              <span>
+                { item.assignment_number }
+              </span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
